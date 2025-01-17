e1419d044466c704f863306f1572f42c1539a0b4
wanchongyu <2459076509@qq.com>
2025-01-18 02:03:59 +0800

    after several tests and experiment I understand pipes better

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,8 +4,8 @@
 int main()
 {
     int p[2], q[2];
-    char Child;
-    char Parent;
+    char Child[2];
+    char Parent[2];
     if (pipe(p) < 0|| pipe(q) < 0)
         exit(1);
     int pid = fork();
@@ -16,23 +16,25 @@ int main()
     }
     else if (pid == 0)
     {
-        close(p[0]);
-        close(q[1]);
-        write(p[1],"a",1);
-        close(p[1]);
-        read(q[0],&Child,1);
-        close(q[0]);
+        //close(p[0]);
+        //close(q[1]);
+        write(p[1],"aaaaaaaaaa",10);
+        //close(p[1]);
+        read(p[0],Child,10);
+        //close(q[0]);
+        printf("child %s\n",Child);
         printf("%d: received ping\n",getpid());
     }
     else
     {
-        close(p[1]);
-        close(q[0]);
-        write(q[1],"a",1);
-        close(q[1]);
-        read(p[0],&Parent,1);
-        close(p[0]);
+        //close(p[1]);
+        //close(q[0]);
+        write(p[1],"bbbbbbbbbb",10);
+        //close(q[1]);
+        read(p[0],Parent,10);
+        //close(p[0]);
         wait(0);
+        printf("Parent %s\n",Parent);
         printf("%d: received pong\n",getpid());
     }
     exit(0);
---
+++ user/primes.c
@@ -0,0 +1,12 @@
+#include <kernel/types.h>
+#include <kernel/stat.h>
+#include <user/user.h>
+void prime(int) __attribute__((noreturn));
+void prime(int){
+
+}
+
+int main(int argc,char* argv[]){
+    
+    exit(0);
+}
