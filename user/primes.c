7ecf59061a1aca9c52fd3f6a4b2d0f54b0e90494
wanchongyu <2459076509@qq.com>
2025-01-18 21:30:30 +0800

    primes failed to output 3

--- user/primes.c
+++ user/primes.c
@@ -1,12 +1,56 @@
 #include <kernel/types.h>
 #include <kernel/stat.h>
 #include <user/user.h>
-void prime(int) __attribute__((noreturn));
-void prime(int){
-
+void prime(int n)
+{
+    int num, sieve, pid;
+    int p[2];
+    if (read(n, &sieve, sizeof(int)) <= 0 || sieve <= 0)
+    {
+        printf("error!!!!\n");
+        exit(1);
+    }
+    printf("prime %d\n", sieve);
+    pipe(p);
+    fork();
+    pid = getpid();
+    if (pid == 0)
+    {
+        close(p[1]);
+        prime(p[0]);
+    }
+    else
+    {
+        close(p[0]);
+        while (read(n, &num, sizeof(int)) && num)
+        {
+            if (num % sieve)
+            {
+                write(p[1], &num, sizeof(int));
+            }
+        }
+    }
 }
-
-int main(int argc,char* argv[]){
-    
+void PrimeStart()
+{
+    int i;
+    int p[2];
+    pipe(p);
+    if (fork() == 0)
+    {
+        for (i = 2; i < 15; i++)
+        {
+            write(p[1], &i, sizeof(int));
+        }
+    }
+    else
+    {
+        close(p[1]);
+        prime(p[0]);
+    }
+}
+int main(int argc, char *argv[])
+{
+    PrimeStart();
     exit(0);
 }
