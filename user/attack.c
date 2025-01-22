471676bc65d59abe10d1b3f6807142690b6006ce
wanchongyu <2459076509@qq.com>
2025-01-22 15:13:06 +0800

    finish attack lab

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,12 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  char *end = sbrk(PGSIZE*32);
+  end = end+32+16*PGSIZE;
+  char secret[9];
+  for(int i = 0;i < 8;i++){
+    secret[i] = end[i];
+  }
+  write(2,secret,8);
   exit(1);
 }
