60c79371708d8973634f236aa25722806328a214
wanchongyu <2459076509@qq.com>
2025-02-04 21:44:41 +0800

    ugetpid optimized

--- kernel/proc.c
+++ kernel/proc.c
@@ -131,7 +131,11 @@ found:
     release(&p->lock);
     return 0;
   }
-
+  if((p->usyscall = (struct usyscall*)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -145,7 +149,7 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
-
+  p->usyscall->pid = p->pid;
   return p;
 }
 
@@ -169,6 +173,8 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  if(p->usyscall) kfree((void*)p->usyscall);
+  p->usyscall = 0;
 }
 
 // Create a user page table for a given process, with no user memory,
@@ -202,6 +208,12 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  if(mappages(pagetable,USYSCALL,PGSIZE,(uint64)(p->usyscall),PTE_R|PTE_U)<0){
+    uvmunmap(pagetable,TRAMPOLINE,1,0);
+    uvmunmap(pagetable,TRAPFRAME,1,0);
+    uvmfree(pagetable,0);
+    return 0;
+  }
   return pagetable;
 }
 
@@ -212,6 +224,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
